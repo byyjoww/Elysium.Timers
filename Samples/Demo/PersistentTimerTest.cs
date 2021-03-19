1c7c049669af0ef3dab5e0dce6d7f57ac8a240b8
jonathan_aykroyd <olympusgate@gmail.com>
2021-03-19 03:29:22 +0000

    add samples

---
+++ Samples/Demo/PersistentTimerTest.cs
@@ -0,0 +1,17 @@
+﻿using Elysium.Timers;
+using Elysium.Utils.Timers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentTimerTest : MonoBehaviour
+{
+    [SerializeField] private PersistentTimerSO persistentTimer = default;
+
+    [ContextMenu("Start timer with 10 seconds")]
+    private void StartTimerWith10()
+    {
+        persistentTimer.StartNewTimer(10f);
+    }
+}
